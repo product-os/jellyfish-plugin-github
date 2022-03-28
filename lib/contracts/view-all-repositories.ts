2c784671cde03bd315f6c544c1af2b9df8f2888c
Lucian Buzzo <lucian.buzzo@gmail.com>
2022-03-28 11:56:12 +0100

    Add a view that shows all repositories
    Change-type: minor
    Signed-off-by: Lucian Buzzo <lucian.buzzo@gmail.com>

---
+++ lib/contracts/view-all-repositories.ts
@@ -0,0 +1,29 @@
+import type { ContractDefinition } from '@balena/jellyfish-types/build/core';
+
+export const viewAllRepositories: ContractDefinition = {
+	slug: 'view-all-repositories',
+	name: 'All Products',
+	type: 'view@1.0.0',
+	markers: ['org-balena'],
+	data: {
+		allOf: [
+			{
+				name: 'Repositories',
+				schema: {
+					type: 'object',
+					properties: {
+						active: {
+							const: true,
+							type: 'boolean',
+						},
+						type: {
+							type: 'string',
+							const: 'repository@1.0.0',
+						},
+					},
+					required: ['active', 'type'],
+				},
+			},
+		],
+	},
+};
