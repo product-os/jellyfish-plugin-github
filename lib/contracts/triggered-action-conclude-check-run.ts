06c514a7707259c9238bbc582a3dd6403f814a37
Thomas Manning <thomasm@balena.io>
2022-06-17 12:44:47 +0000

    Update triggers that use link 'has attached check run' to 'has attached'
    Change-type: patch

--- lib/contracts/triggered-action-conclude-check-run.ts
+++ lib/contracts/triggered-action-conclude-check-run.ts
@@ -9,7 +9,7 @@ export const triggeredActionConcludeCheckRun: ContractDefinition = {
 		schedule: 'sync',
 		filter: {
 			$$links: {
-				'has attached check run': {
+				'has attached': {
 					type: 'object',
 					required: ['active', 'type'],
 					properties: {
@@ -66,7 +66,7 @@ export const triggeredActionConcludeCheckRun: ContractDefinition = {
 		action: 'action-update-card@1.0.0',
 		target: {
 			$map: {
-				$eval: "source.links['has attached check run']",
+				$eval: "source.links['has attached']",
 			},
 			'each(link)': {
 				$eval: 'link.id',
