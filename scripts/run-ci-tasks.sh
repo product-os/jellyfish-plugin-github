b76845fa482e545fce7807e61c3ff8eebdee5f4f
Lucian Buzzo <lucian.buzzo@gmail.com>
2022-07-11 11:42:16 +0100

    Use flowzone for SUT testing
    Change-type: patch
    Signed-off-by: Lucian Buzzo <lucian.buzzo@gmail.com>

--- scripts/run-ci-tasks.sh
+++ scripts/run-ci-tasks.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-# Set necessary environment variables
-export INTEGRATION_GITHUB_APP_ID=$(cat /run/secrets/integration_github_app_id)
-export INTEGRATION_GITHUB_PRIVATE_KEY=$(cat /run/secrets/integration_github_private_key)
-export INTEGRATION_GITHUB_TOKEN=$(cat /run/secrets/integration_github_token)
-
 # Run tasks
 scripts/close-test-issues.js || true
 npm run test:integration
