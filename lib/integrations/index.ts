a65f5d0288c60864b98a64351471f3d9a980fb06
Josh Bowling <josh@balena.io>
2023-12-16 12:20:49 +0900

    Remove sync integration
    Change-type: major

--- lib/integrations/index.ts
+++
@@ -1,6 +0,0 @@
-import type { IntegrationDefinition, Map } from '@balena/jellyfish-worker';
-import { githubIntegrationDefinition } from './github';
-
-export const integrations: Map<IntegrationDefinition> = {
-	github: githubIntegrationDefinition,
-};
--- lib/integrations/utils.ts
+++
@@ -1,3 +0,0 @@
-export function isEmail(value: string): boolean {
-	return /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(value);
-}
